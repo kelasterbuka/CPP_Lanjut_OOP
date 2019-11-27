d194c1f9cce7f899d3b0745000eac31948dfb483
Faqihza UNSW from SAVE LAB <faqihza.m@gmail.com>
2019-11-27 13:10:13 +1100

    Ubuntu added

---
+++ 00 - Project template untuk Ubuntu/src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello world" << endl;
+    cout << "instalasi berhasil" << endl;
+    return 0;
+}
